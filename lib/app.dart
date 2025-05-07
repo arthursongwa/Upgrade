93bd8c7f885c346ffdc3b87752bf817277fb3867
Arthur Songwa <114635747+arthursongwa@users.noreply.github.com>
2025-05-07 11:57:04 +0200

    Creation du systeme de routage et initialisation des premiers ecrans

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:upgrade/core/theme.dart';
+import 'package:upgrade/core/router.dart'; // ← le router défini
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,
+      // theme: AppTheme.darkTheme, // si tu as défini un thème sombre
+    );
+  }
+}
---
+++ lib/core/router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:upgrade/ui/screens/home_screen.dart';
+import 'package:upgrade/ui/screens/nutrition_screen.dart';
+import 'package:upgrade/ui/screens/profile_screen.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: 'home',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/nutrition',
+      name: 'nutrition',
+      builder: (context, state) => const NutritionScreen(),
+    ),
+    GoRoute(
+      path: '/settings',
+      name: 'settings',
+      builder: (context, state) => const ProfileScreen(),
+    ),
+  ],
+);
